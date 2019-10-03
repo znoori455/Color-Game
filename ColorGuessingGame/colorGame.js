d66e652cb3dfedd98fb412ea3ef79646e6f5c782
znoori455 <znoori455@gmail.com>
2019-10-02 21:25:35 -0400

    functionality complete; parts 1-6

--- ColorGuessingGame/colorGame.js
+++ ColorGuessingGame/colorGame.js
@@ -1,4 +1,5 @@
-let colors = generateRandomColors(6);
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
@@ -6,7 +7,55 @@ let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 
-colorDisplay.textContent = pickedColor
+let resetButton = document.querySelector("#reset");
+let easyBtn = document.querySelector("#easyBtn");
+let hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(let i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function() {
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(let i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+    }
+});
+
+
+resetButton.addEventListener("click", function() {
+    //generate all new colors
+    colors = generateRandomColors(numSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change  colorDisplay to match picked Color
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares on page
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+});
+
+colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -20,6 +69,7 @@ for(let i = 0; i < squares.length; i++) {
         //compare color to picked color
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
             changeColors(clickedColor);
             h1.style.backgroundColor = pickedColor;
         } else {
